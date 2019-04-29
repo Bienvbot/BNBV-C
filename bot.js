9e73e4286ab87254e14ea449af7a2b0613f70f48
Bienvbot <50112140+Bienvbot@users.noreply.github.com>
2019-04-29 18:42:43 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,42 @@
+const { Client, Util } = require('discord.js');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+
+const client = new Client({ disableEveryone: true });
+const GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
+const youtube = new YouTube(GOOGLE_API_KEY);
+
+const queue = new Map();
+const PREFIX = '=';
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+client.on('ready', () => { 
+console.log(`
+------------------------------------------------------
+> Logging in...
+------------------------------------------------------
+Logged in as ${client.user.tag}
+Working on ${client.guilds.size} servers!
+${client.channels.size} channels and ${client.users.size} users cached!
+I am logged in and ready to roll!
+LET'S GO!
+------------------------------------------------------
+-------------------------------------------------------
+------------------------------------------------------
+----------------------Bot's logs----------------------`);
+
+
+});
+
+
+client.on('ready', () => {
+    console.log('Bot Is Ready Now !');
+    client.user.setGame(`${PREFIX}help`,"http://twitch.tv/xkilleryt")
+    });
+
+
+
+
+client.login(process.env.BOT_TOKEN);
